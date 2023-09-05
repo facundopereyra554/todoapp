b1b3101c417e6877477683bc03e59a4f18555445
Luis-AP <lap18958@gmail.com>
2023-09-05 16:41:26 -0300

    Added taskCounter to the category cards. Added new style to this counter"

--- scripts/category.js
+++ scripts/category.js
@@ -1,29 +1,46 @@
 let categoryBox = document.querySelector(".categoryBox");
 let categoryBtn = document.querySelector(".addCategory");
 
+const getTaskCount = async (categoryId) => {
+    // Fetch tasks for the specific category and return the count
+    const res = await fetch(`http://127.0.0.1:5000/tasks/?category_id=${categoryId}`);
+    const data = await res.json();
+    return data.length;
+};
+
 let catchCategories = () => {
     fetch("http://127.0.0.1:5000/categories/")
     .then(res => res.json())
-    .then(data => {
-        data.map(categoria => {
+    .then(async data => {
+        for (let categoria of data) {
+            const taskCount = await getTaskCount(categoria.category_id);
+            
+            // Create category text element
             let taskText = document.createElement("a");
-            taskText.classList.add("categoryText"); 
+            taskText.classList.add("categoryText");
             taskText.setAttribute("href", `tasks.html?category_id=${categoria.category_id}`);
             taskText.setAttribute("data-category-id", categoria.category_id);
             taskText.textContent = categoria.name;
+
+            // Create due task span
             let dueTaskSpan = document.createElement("span");
             let categoryDescription = `Descripción: ${categoria.description}`;
             dueTaskSpan.textContent = categoryDescription;
             dueTaskSpan.classList.add("dueTaskSpan");
+            
+            // Create task count circle
+            let taskCountCircle = document.createElement("div");
+            taskCountCircle.classList.add("taskCountCircle");
+            taskCountCircle.textContent = taskCount;  // Here, taskCount is dynamically fetched
+            
+            // Append elements
             taskText.appendChild(document.createElement("br"));
             taskText.appendChild(dueTaskSpan);
+            taskText.appendChild(taskCountCircle);  // Append the task count circle
             categoryBox.appendChild(taskText);
-        });
+        }
 
-        categoryBtn.addEventListener("click", () => addCategory())
-
-
-
+        categoryBtn.addEventListener("click", () => addCategory());
     })
     .catch(err => console.log(err));
 };
@@ -50,9 +67,3 @@ let addCategory = () => {
     })
     .catch(err => console.log(err));
 }
-
-
-
-
-
-
